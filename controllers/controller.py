fe29511887aba84b19d3b9a5e3ed237392f4e53f
sailorj94 <jackniall9@gmail.com>
2021-04-16 12:03:20 +0100

    Game Logic Added

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,11 @@
 from flask import render_template
 from app import app
+from models.game import *
+from models.player import *
 
 @app.route('/')
 def index():
     return(render_template('index.html'))
+
+@app.route('/<player1>/<player2>')
+def rps():
