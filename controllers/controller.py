5319ceca9d4ec26911cb3ba975dc9b1eea137f77
sailorj94 <jackniall9@gmail.com>
2021-04-17 08:25:30 +0100

    Template inheritance added, pvp forms and game logic added and working

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,11 +1,34 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
-from models.game import Game
-from models.player import Player
+from models.game import *
+from models.player import *
+
+# Redirect from default landing page to '/welcome' where the game starts.
+@app.route('/')
+def home():
+    return redirect(url_for('index'))
 
 @app.route('/welcome')
 def index():
     return(render_template('index.html'))
+
+# Both game methods still playable.
+
+@app.route('/welcome', methods = ['POST'])
+def pvp_game():
+    new_p1_name = request.form['player1_name']
+    new_p1_choice = request.form['player1_choice']
+    new_p2_name = request.form['player2_name']
+    new_p2_choice = request.form['player2_choice']
+    player1 = Player(new_p1_name, new_p1_choice)
+    player2 = Player(new_p2_name, new_p2_choice)
+    game = Game(player1, player2)
+    win = game.run_rps()
+    lose = game.rps_loser()
+    if (win =='draw' or lose == "draw"):
+        return (render_template('draw.html'))
+
+    return(render_template('result.html', winner = win, loser = lose))
 
 @app.route('/<choice1>/<choice2>')
 def rps_game(choice1, choice2):
@@ -16,5 +39,5 @@ def rps_game(choice1, choice2):
     lose = game.rps_loser()
     if (win =='draw' or lose == "draw"):
         return (render_template('draw.html'))
-    # return f'{win.name} wins by playing {win.choice}!'
+
     return (render_template('result.html', winner = win, title = 'And the winner is...', loser = lose))
